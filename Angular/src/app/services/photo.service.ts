a2c53c3ebd23bf312d9815393691d31f9eed9968
Louis Xie <louis@Louis-MacBook-Pro.local>
2021-12-12 22:06:19 +0800

    add plugin

---
+++ Angular/src/app/services/photo.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Storage } from '@capacitor/storage';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+
+  constructor() { }
+}
